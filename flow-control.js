52c31f11adbdda3f4ac6914657839488eff5ff2e
Murali Kesavan <murali@advidh.com>
2019-03-25 03:19:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,17 @@
 function basicTeenager(age) {
-
+  // learning if-elseif-then
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  } 
 }
 
 function teenager(age) {
-
+  // learning if-elsen
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  } else if (age < 13) {
+    return "You are not a teenager!";
+  }  
 }
 
 function ageChecker(age) {
@@ -14,7 +22,7 @@ function ageChecker(age) {
     return "You are a kid!";
   } else {
     return "You are a grownup!";
-}
+  }
 }
 
 function ternaryTeenager(age) {
